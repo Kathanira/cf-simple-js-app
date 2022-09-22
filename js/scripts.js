a790306762999862e2c575a95820edb65e6d007e
Kathanira <112494140+Kathanira@users.noreply.github.com>
2022-09-22 10:14:41 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName); // Everything after the slashes is ignored
+/* This is a longer comment.
+It might explain, in detail, why we made a certain decision.
+Or, it could show examples about how to use the following code snippet. */
+
