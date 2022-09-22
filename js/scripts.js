51cfb3da7616984ccf291e871f36eebae3274c61
Kathanira <112494140+Kathanira@users.noreply.github.com>
2022-09-22 10:27:09 +0200

    Added some variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
-let myName = 'Bob';
+alert('Hello world');
+
+let myName = 'Katharina';
 document.write(myName);
-myName = 'John Doe';
-document.write(myName); // Everything after the slashes is ignored
+document.write("<br>");
+// Everything after the slashes is ignored
 /* This is a longer comment.
 It might explain, in detail, why we made a certain decision.
 Or, it could show examples about how to use the following code snippet. */
 
+let favoriteFood = 'pizza';
+document.write(favoriteFood);
