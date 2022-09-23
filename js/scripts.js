002ecda9a71fde614a896742f57f879754270375
Kathanira <112494140+Kathanira@users.noreply.github.com>
2022-09-23 14:44:12 +0200

    Added a loop and if/else

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,17 @@
 let pokemonList = [
     { name: 'Balbasure', height: 7, type: ['grass', 'poison'] },
     { name: 'Charizard', height: 2, type: ['fire', 'flying'] },
-    { name: 'Mightyena', height: 1, type: 'dark' }
+    { name: 'Mightyena', height: 1.7, type: 'dark' }
 ];
+for (let i = 0;
+    i < pokemonList.length; i++) {
+    if(pokemonList[i].height >= 3) {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - Wow, that is big!" + "<br>")
+    }
+    else if (pokemonList[i].height >= 1 && pokemonList[i].height <3){
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - That is a medium pokemon!" + "<br>")
+    }
+    else {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - That is a small pokemon!" + "<br>")
+    }
+}
