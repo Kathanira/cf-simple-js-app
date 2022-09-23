cd2dde8204751239e382ea799bab8f14f0399085
Kathanira <112494140+Kathanira@users.noreply.github.com>
2022-09-23 08:35:40 +0200

    Updated the pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,5 @@
-
-// First Task
-alert('Hello world');
-
-let myName = 'Katharina';
-document.write(myName);
-document.write("<br>");
-// Everything after the slashes is ignored
-/* This is a longer comment.
-It might explain, in detail, why we made a certain decision.
-Or, it could show examples about how to use the following code snippet. */
-
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
-
-// Second Task
-/*FYI I have left the top code because the first task has not been approved yet. 
-Will delete the first part once the first task is completed*/
-
-let pokemonList=[
-    {name:"Balbasure",height:7, type:['grass','poison']}, 
-    {name:"Charizard", height:2, type:['fire','flying']}, 
-    {name:"Mightyena", height:1, type:'dark'},
+let pokemonList = [
+    { name: 'Balbasure', height: 7, type: ['grass', 'poison'] },
+    { name: 'Charizard', height: 2, type: ['fire', 'flying'] },
+    { name: 'Mightyena', height: 1, type: 'dark' }
 ];
-
-
