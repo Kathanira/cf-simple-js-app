c1a50702b49455bb63aa67c94260a16c2bf8f52d
Kathanira <112494140+Kathanira@users.noreply.github.com>
2022-09-28 09:01:16 +0200

    Updated the forEach Loop

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,19 @@ for (let i = 0;
 
 let pokemonRepository =(function(){
     let pokemonList = [
-        { name: 'Balbasure', height: 7, type: ['grass', 'poison'] },
-        { name: 'Charizard', height: 2, type: ['fire', 'flying'] },
-        { name: 'Mightyena', height: 1.7, type: 'dark' }
+        { name: 'Balbasure', 
+        height: 7, 
+        type: ['grass', 'poison'] },
+
+        { name: 'Charizard', 
+        height: 2, 
+        type: ['fire', 'flying'] },
+
+        { name: 'Mightyena', 
+        height: 1.7, 
+        type: 'dark' }
     ]
+
     function getAll() {
         return pokemonList;
       }
@@ -39,5 +48,22 @@ let pokemonRepository =(function(){
       }
 
 })()
+
+pokemonRepository.add({name:'Fearow', height: 1.2, type:['normal', 'flying']});
+
+
+pokemonRepository.getAll().forEach(function(pokemon){
+
+    if(pokemon.height >= 5) {
+        document.write(pokemon.name + " (height: " + pokemon.height + " m) - Wow, that is big!" + "<br>")
+    }
+    else if (pokemon.height >= 2 && pokemon.height < 5){
+        document.write(pokemon.name + " (height: " + pokemon.height + " m) - That is a medium pokemon!" + "<br>")
+    }
+    else {
+        document.write(pokemon.name + " (height: " + pokemon.height + " m)- That is a small pokemon!" + "<br>")
+    }  
+})
+
 
 
